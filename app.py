d2fa3563ffb75532efd1f0e44b4e6050b3a6dffe
lvruiza1343 <lauraruizalvarez70@gmail.com>
2025-04-10 14:06:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ st.write("🧠 Versión de Python:", platform.python_version())
 # Imagen decorativa
 try:
     image = Image.open('piton.jpg')
-    st.image(image, width=350)
+    st.image(image, width=550)
 except Exception as e:
     st.warning(f"⚠️ No se pudo cargar la imagen: {e}")
 
